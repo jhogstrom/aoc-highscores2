4ca89db9a144ed7720e577882fd32d8e6c494f04
Jesper Högström <jesper.hogstrom@aditrologistics.com>
2021-11-13 23:55:23 +0100

    Added vuetify and a plain highscore list

--- frontend/src/components/MainTable.vue
+++ frontend/src/components/MainTable.vue
@@ -1,29 +1,54 @@
 <template>
 <div>
+
+
   <h1>AOC Highscores</h1>
   {{ name }}
   {{ year }}
+  <plain-highscore-list :players="plainplayers"/>
   <p v-if="loadedOk">
-  {{ data }}
+    <v-expansion-panels>
+      <v-expansion-panel>
+        <v-expansion-panel-header>
+          raw data
+        </v-expansion-panel-header>
+        <v-expansion-panel-content>
+        {{ data }}
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+
   </p>
   </div>
 </template>
 
 <script>
+import PlainHighscoreList from './PlainHighscoreList.vue'
+var _ = require('lodash');
+
 const ROOTURL="https://aochsstack-website.s3.us-east-2.amazonaws.com"
 // https://aochsstack-website.s3.us-east-2.amazonaws.com/2020/7d3e8718-f15c-41ed-a561-fbba4f3fa37c.json
 export default {
+    components: { PlainHighscoreList },
     props: ["guid", "year"],
     data() { return {
         name: "hello",
         data: {},
-        loadedOk: false
+        loadedOk: false,
+        comment: "foobar"
     }},
     mounted() {
         this.name = "foobar"
         // this.fetchData(2020, "7d3e8718-f15c-41ed-a561-fbba4f3fa37c")
         this.fetchData(this.year, this.guid)
 
+    },
+    computed: {
+      plainplayers() {
+        console.log(this.data.Players)
+        return _.sortBy(this.data.Players, ["LocalScore"]).slice().reverse();
+
+      }
     },
     methods: {
         makeUrl(year, guid) {
---
+++ frontend/src/components/PlainHighscoreList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <div v-for="player in players" :key="player.id">
+          <p>{{player.Name}} - {{player.LocalScore}}</p>
+      </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        players: []
+    }
+}
+</script>
+
+<style>
+
+</style>
