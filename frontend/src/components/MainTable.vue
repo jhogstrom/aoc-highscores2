507fe8f0d41c2f296ecdb420248167ffe619da17
Jesper Högström <jesper.hogstrom@aditrologistics.com>
2021-11-14 20:25:38 +0100

    Pre-add store

--- frontend/src/components/MainTable.vue
+++ frontend/src/components/MainTable.vue
@@ -1,11 +1,45 @@
 <template>
 <div>
+  <v-card>
+    <v-toolbar
+      color="cyan"
+      dark
+      flat
+    >
+      <v-toolbar-title>AOC Highscores</v-toolbar-title>
+      <template v-slot:extension>
+          <v-tabs
+            v-model="tab"
+            align-with-title
+          >
+            <v-tabs-slider color="yellow"></v-tabs-slider>
 
+            <v-tab
+              v-for="item in items"
+              :key="item.title"
+            >
+              {{ item.title }}
+            </v-tab>
+          </v-tabs>
+        </template>
+      </v-toolbar>
+      <v-tabs-items v-model="tab">
+        <v-tab-item
+          v-for="item in items"
+          :key="item.title"
+        >
+          <v-card flat>
+            <component v-bind:is="item.component" v-bind="{...item.props}"></component>
+            <!-- <v-card-text v-text="text"></v-card-text> -->
+          </v-card>
+        </v-tab-item>
+      </v-tabs-items>
+    </v-card>
 
-  <h1>AOC Highscores</h1>
-  {{ name }}
-  {{ year }}
   <plain-highscore-list :players="plainplayers"/>
+  <h1>AOC Highscores</h1>
+  <!-- {{ name }}
+  {{ year }}
   <p v-if="loadedOk">
     <v-expansion-panels>
       <v-expansion-panel>
@@ -18,7 +52,7 @@
       </v-expansion-panel>
     </v-expansion-panels>
 
-  </p>
+  </p> -->
   </div>
 </template>
 
@@ -32,12 +66,17 @@ export default {
     components: { PlainHighscoreList },
     props: ["guid", "year"],
     data() { return {
+        tab: null,
         name: "hello",
         data: {},
         loadedOk: false,
-        comment: "foobar"
+        comment: "foobar",
+        items: [
+          {title: "highscore", component: "PlainHighscoreList", props: {players: this.plainplayers}},
+          {title: "highscore2", component: "PlainHighscoreList", props: {players: "plainplayers"}},
+        ]
     }},
-    mounted() {
+    created() {
         this.name = "foobar"
         // this.fetchData(2020, "7d3e8718-f15c-41ed-a561-fbba4f3fa37c")
         this.fetchData(this.year, this.guid)
--- frontend/src/components/PlainHighscoreList.vue
+++ frontend/src/components/PlainHighscoreList.vue
@@ -2,6 +2,7 @@
   <div>
       <div v-for="player in players" :key="player.id">
           <p>{{player.Name}} - {{player.LocalScore}}</p>
+          XXX
       </div>
 
   </div>
@@ -11,6 +12,9 @@
 export default {
     props: {
         players: []
+    },
+    mounted() {
+        console.log("XX", this.players)
     }
 }
 </script>
