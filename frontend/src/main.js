eaf41466b960d356aaf13670f8182cf77f080c84
Jesper Högström <jesper.hogstrom@aditrologistics.com>
2021-11-29 20:45:05 +0100

    Added support for google analytics. Maybe...

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,8 +3,13 @@ import App from './App.vue'
 import vuetify from '@/plugins/vuetify' // path to vuetify export
 import store from './store/index'
 import router from './router'
+import VueGtag from "vue-gtag"
 
 Vue.config.productionTip = false
+
+Vue.use(VueGtag, {
+  config: { id: "G-ZWGPG7GN8G" }
+})
 
 new Vue({
   vuetify,
