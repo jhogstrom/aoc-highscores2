1fc92a0bc26d461572b8e4c62428abc0091f79bb
Jesper Högström <jesper.hogstrom@aditrologistics.com>
2021-12-10 07:36:03 +0100

    Reverted PositionCol to restore positions in identity column

--- frontend/src/components/PositionCol.vue
+++ frontend/src/components/PositionCol.vue
@@ -3,27 +3,15 @@
         <template v-slot:activator="{ on, attrs }">
             <span v-bind="attrs" v-on="on">{{item.position}}</span>
         </template>
-       <span>{{item.name}}<br>*1: {{ solveTime(item, 0) }}<br>*2: {{ solveTime(item, 1) }}</span>
+        <span>{{item.name}}</span>
     </v-tooltip>
 
 </template>
 
 <script>
-import { getMedalColor, getSolveTime } from './tablehelpers'
-
 export default {
-    props: ["item", "header", "getValue"],
-    methods: {
-        medalColor(item) {
-            const posKey = "s" + this.header.slice(1)
-            if (item[posKey] !== -1)
-                return getMedalColor(item, this.header)
-            return "incompleteStar"
-        },
-        solveTime(item, star) {
-            return getSolveTime(item, this.header, star)
-        },
-    }
+    props: ["item"]
+
 }
 </script>
 
